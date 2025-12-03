31d038d2b8f52fc02a26f51d3de28ff7f41762b5
Michael Woodvine <michael.woodvine@spryker.com>
2025-12-03 01:07:20 +0100

    Initial Commit

---
+++ TrainingLogV2/TrainingLogV2App.swift
@@ -0,0 +1,17 @@
+//
+//  TrainingLogV2App.swift
+//  TrainingLogV2
+//
+//  Created by Michael Woodvine on 03.12.25.
+//
+
+import SwiftUI
+
+@main
+struct TrainingLogV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
